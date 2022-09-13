b226c272ae20f6a3fc6950c3c0d33612620f0c95
powerspieler <pascal.houy@web.de>
2022-09-13 21:46:51 +0200

    Added Altar Book - Discovery

--- src/main/java/me/powerspieler/paveral/util/RecipeLoader.java
+++ src/main/java/me/powerspieler/paveral/util/RecipeLoader.java
@@ -1,6 +1,8 @@
 package me.powerspieler.paveral.util;
 
 import me.powerspieler.paveral.Paveral;
+import me.powerspieler.paveral.discovery.Discovery;
+import me.powerspieler.paveral.discovery.tutorial.altar_book;
 import me.powerspieler.paveral.items.Chunkloader;
 import me.powerspieler.paveral.items.Items;
 import me.powerspieler.paveral.items.Wrench;
@@ -25,5 +27,14 @@ public class RecipeLoader {
         clRecipe.setIngredient('O', Material.OBSIDIAN);
         clRecipe.setIngredient('E', Material.ENCHANTING_TABLE);
         Bukkit.addRecipe(clRecipe);
+
+        Discovery altar_book = new altar_book();
+        ShapedRecipe al_Recipe = new ShapedRecipe(new NamespacedKey(Paveral.getPlugin(), "altar_book"), altar_book.build());
+        al_Recipe.shape(" C ","S+S","#S#");
+        al_Recipe.setIngredient('C', Material.AMETHYST_CLUSTER);
+        al_Recipe.setIngredient('S', Material.AMETHYST_SHARD);
+        al_Recipe.setIngredient('+', Material.BOOK);
+        al_Recipe.setIngredient('#', Material.AMETHYST_BLOCK);
+        Bukkit.addRecipe(al_Recipe);
     }
 }
