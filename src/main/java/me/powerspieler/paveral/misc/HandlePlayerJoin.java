1c2935d2c5ba032e8ffb39c58153617ccf71a1e7
powerspieler <pascal.houy@web.de>
2022-09-26 23:06:59 +0200

    Implemented Recipe and Finishing Touches

--- src/main/java/me/powerspieler/paveral/misc/HandlePlayerJoin.java
+++ src/main/java/me/powerspieler/paveral/misc/HandlePlayerJoin.java
@@ -1,13 +1,16 @@
 package me.powerspieler.paveral.misc;
 
+import me.powerspieler.paveral.Paveral;
 import me.powerspieler.paveral.advancements.AwardAdvancements;
 import me.powerspieler.paveral.util.RecipeLoader;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.persistence.PersistentDataType;
 
 public class HandlePlayerJoin implements Listener {
     @EventHandler
@@ -21,9 +24,18 @@ public class HandlePlayerJoin implements Listener {
     @EventHandler
     public void onPJChangelog(PlayerJoinEvent event){
         Player player = event.getPlayer();
+        NamespacedKey msg = new NamespacedKey(Paveral.getPlugin(), "show_chlog");
+        if(!player.getPersistentDataContainer().has(msg)){
+            player.getPersistentDataContainer().set(msg, PersistentDataType.INTEGER, 3);
+        }
+        if(player.getPersistentDataContainer().get(msg, PersistentDataType.INTEGER) != null && player.getPersistentDataContainer().get(msg, PersistentDataType.INTEGER) > 0){
+            player.sendMessage(Component.text("\"Paveralicious Additions - Datapack\" has been ported to a Java-Plugin \"Paveral\" due to performance reasons!", NamedTextColor.GOLD));
+            player.sendMessage(Component.text("Every item / tool crafted so far will NOT work any longer and need to be recrafted! Some tweaks have also been made:", NamedTextColor.DARK_PURPLE));
+            player.sendMessage(Component.text("+ Added a new multistructure to craft tech-related items\n* Behaviour of Lightning Rod has been tweaked\n* Many many more minor tweaks\n- Removed Golden Crook (Sheepfreeze)", NamedTextColor.YELLOW));
 
-        player.sendMessage(Component.text("\"Paveralicious Additions - Datapack\" has been ported to a Java-Plugin \"Paveral\" due to performance reasons!", NamedTextColor.GOLD));
-        player.sendMessage(Component.text("Every item / tool crafted so far will NOT work any longer and need to be recrafted! Some tweaks have also been made:", NamedTextColor.DARK_PURPLE));
-        player.sendMessage(Component.text("+ Added a new multistructure to craft tech-related items\n* Behaviour of Lightning Rod has been tweaked\n* Many many more minor tweaks\n- Removed Golden Crook (Sheepfreeze)", NamedTextColor.YELLOW));
+            int msg_remain = player.getPersistentDataContainer().get(msg, PersistentDataType.INTEGER);
+            msg_remain --;
+            player.getPersistentDataContainer().set(msg, PersistentDataType.INTEGER, msg_remain);
+        }
     }
 }
